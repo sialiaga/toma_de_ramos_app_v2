738dcc0c8781d22383f0c7d162a95f23f276e711
Samuel Aliaga Gleisner <sialiaga@miuandes.cl>
2023-12-12 22:45:51 -0300

    fix: pre solve

--- toma_de_ramos_app/apps.py
+++ toma_de_ramos_app/apps.py
@@ -5,6 +5,6 @@ class TomaDeRamosAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'toma_de_ramos_app'
 
-    def ready(self):
-        from . import cargar_ramos
-        cargar_ramos.cargar_ramos()
+    # def ready(self):
+    #     from . import cargar_ramos
+    #     cargar_ramos.cargar_ramos()
